79dc80af5e1c77089496aa2c32f627c6cf4ee762
Jenny <wujenny214@gmail.com>
2024-10-07 03:27:43 -0400

    fixing test

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ lint:
 	ruff check *.py test_files/test_*.py preprocess_SQL_files/*.py
 	
 test:
-	python -m pytest -vv test_files/test_*.py 
+	python -m pytest -vv preprocess_SQL_files/test_*.py 
 
 check:
 	python main.py
---
+++ preprocess_SQL_files/test_transform_data.py
@@ -0,0 +1,13 @@
+import sys
+from transform_data import transform
+
+"""This file takes the csv data and converts it into a database/.db file"""
+
+
+def test_transform():
+    transform_result = transform()
+    assert transform_result is not None
+
+
+if __name__ == "__main__":
+    test_transform()
